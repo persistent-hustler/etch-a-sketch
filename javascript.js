eacc8d51bf0e3f15aa8cff1fba6b09c27345bee2
Vineeth <145092290+persistent-hustler@users.noreply.github.com>
2024-01-31 20:40:43 +0530

    Let user choose number of cells in a row

--- javascript.js
+++ javascript.js
@@ -1,22 +1,44 @@
 const container = document.querySelector('#container');
-const rowsTotal = 16;
-const columnsTotal = rowsTotal;
-const reset = document.querySelector("#reset");
-reset.addEventListener('click', () => location.reload());
+let cellSize = '40px';
+let rowsTotal = 16;
+let columnsTotal = rowsTotal;
+makeGrid();
 
-for(let row = 1; row <= rowsTotal ; row++) {
-    let gridRow = document.createElement('div');
-    gridRow.classList.add('gridRow');
-    for(let column = 1; column <= columnsTotal ; column++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        gridRow.append(cell);
+const cellsPerRow = document.querySelector("#cellsPerRow");
+cellsPerRow.addEventListener('click', () => {
+    rowsTotal = parseInt(prompt("Enter cells per row(max 100): ", 16));
+    if(rowsTotal <= 100){
+        columnsTotal = rowsTotal;
+        container.innerHTML = '';
+        cellSize = `${640/rowsTotal}px`;
+        makeGrid();
+    } else {
+        alert("Enter number upto 100");
     }
-    container.append(gridRow);
-}
+});
 
-container.addEventListener("mouseover", (event) => {
-    let target = event.target;
-    target.style.backgroundColor = "black";
-    console.log(target);
-})
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
+});
+
+function makeGrid() {
+    for(let row = 1; row <= rowsTotal ; row++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('gridRow');
+        for(let column = 1; column <= columnsTotal ; column++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.setAttribute('style', `width: ${cellSize}; height: ${cellSize};`);
+            gridRow.append(cell);
+        }
+        container.append(gridRow);
+    }
+    container.addEventListener("mouseover", (event) => {
+        let target = event.target;
+        target.style.backgroundColor = "black";
+    });
+}
